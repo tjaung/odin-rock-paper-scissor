07d111aecc7a3291128f37b7b8414c8996ffed4e
tjaung <jaungt@gmail.com>
2021-12-26 23:50:01 -0500

    first commit: saving files in case of crash

---
+++ game.js
@@ -0,0 +1,12 @@
+function computerPlay() {
+    // generate random number
+}
+
+function playRound(playerSelection) {
+    // get player option and computer selection.
+    
+  }
+  
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
